c15324d7a3126fd30e63a963d63a868deb0fd1a7
Martin <martinme447@gmail.com>
2026-06-01 19:33:03 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true, // nativní page transitions
+    serverActions: {
+      bodySizeLimit: "24mb",
+    },
   },
   images: {
     remotePatterns: [
